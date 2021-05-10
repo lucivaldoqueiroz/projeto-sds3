a1ab5142838d8204de5075852e2a2438c1dec851
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2021-05-10 10:57:53 -0300

    trocando endpoint

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,7 +14,7 @@ const DonutChart = () => {
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        axios.get(`${BASE_URL}/sales/sum-by-seller`)
             .then((response) => {
                 const data = response.data as SaleSum[];
                 const myLabels = data.map(x => x.sellerName);
@@ -23,16 +23,6 @@ const DonutChart = () => {
                 setChartData({ labels: myLabels, series: mySeries });
             });
     } , []);
-
-    //FORMA ERRADA
-    //let chartData : ChartData = { labels: [], series: []};
-
-
-
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
 
     const options = {
         legend: {
