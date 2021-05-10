8f9d63b4d377865343f8e63200d8d6b586b8b37e
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2021-05-10 09:59:09 -0300

    Voltando o donutchart

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -6,6 +6,7 @@ import NavBar from "components/NavBar";
 
 const Dashboard = () => {
     return (
+        <div>
             <>
                 <NavBar />
                 <div className="container">
@@ -32,6 +33,7 @@ const Dashboard = () => {
                 </div>
                 <Footer />
             </>
+        </div>
     );
 }
 
